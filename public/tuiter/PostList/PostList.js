3c82d90d3148a8b29109c9db4e732a69cdda505b
Jiaxi Wen <taurreswen@gmail.com>
2022-03-28 23:07:07 -0400

    a7 redux sp22 fix bug

--- public/tuiter/PostList/PostList.js
+++
@@ -1,9 +0,0 @@
-import PostItem from "./PostItem.js";
-import posts from "./posts.js";
-
-const PostList = () => {
-    return(`
-        ${posts.map((post) => PostItem(post)).join('')}
-    `);
-}
-export default PostList;
--- public/tuiter/PostSummaryList/index.js
+++
@@ -1,9 +0,0 @@
-import PostSummaryListItem from "./PostSummaryItem.js";
-import posts from "./posts.js";
-
-const PostSummaryList = () => {
-    return (`
-        ${posts.map((post) => PostSummaryListItem(post)).join('')}
-    `);
-}
-export default PostSummaryList;
